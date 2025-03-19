35d7f87ffccfbe92a1fd53e13b1401dc1bca8c39
MargueriteAnne <margaridacv81@hotmail.com>
2025-03-19 13:40:27 +0000

    connection type fix

--- src/main/java/com/operetta/model/Connection.java
+++ src/main/java/com/operetta/model/Connection.java
@@ -1,36 +1,109 @@
 package com.operetta.model;
-import com.operetta.model.ConnectionType;
-
 
 /**
- * Enum representing the three types of connections in the database
+ * Model class representing a connection between a work and a creator
  */
-public enum ConnectionType {
-    MUSIC("music"),
-    LIBRETTO("libretto"),
-    TRANSLATION("translation");
+public class Connection {
+    private int id;
+    private int workId;
+    private ConnectionType connectionType;
+    private int creatorId;
     
-    private final String value;
+    // These fields are for joining with other tables
+    private String workTitle;
+    private String creatorName;
     
-    ConnectionType(String value) {
-        this.value = value;
-    }
-    
-    public String getValue() {
-        return value;
+    /**
+     * Default constructor
+     */
+    public Connection() {
     }
     
     /**
-     * Convert a string value to its corresponding ConnectionType
-     * @param value The string value
-     * @return The ConnectionType enum value
+     * Constructor with all fields
      */
-    public static ConnectionType fromString(String value) {
-        for (ConnectionType type : ConnectionType.values()) {
-            if (type.value.equalsIgnoreCase(value)) {
-                return type;
-            }
-        }
-        throw new IllegalArgumentException("Unknown connection type: " + value);
+    public Connection(int id, int workId, ConnectionType connectionType, int creatorId) {
+        this.id = id;
+        this.workId = workId;
+        this.connectionType = connectionType;
+        this.creatorId = creatorId;
+    }
+    
+    /**
+     * Constructor with joined data
+     */
+    public Connection(int id, int workId, ConnectionType connectionType, int creatorId,
+                     String workTitle, String creatorName) {
+        this.id = id;
+        this.workId = workId;
+        this.connectionType = connectionType;
+        this.creatorId = creatorId;
+        this.workTitle = workTitle;
+        this.creatorName = creatorName;
+    }
+    
+    // Getters and setters
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public int getWorkId() {
+        return workId;
+    }
+    
+    public void setWorkId(int workId) {
+        this.workId = workId;
+    }
+    
+    public ConnectionType getConnectionType() {
+        return connectionType;
+    }
+    
+    public void setConnectionType(ConnectionType connectionType) {
+        this.connectionType = connectionType;
+    }
+    
+    public int getCreatorId() {
+        return creatorId;
+    }
+    
+    public void setCreatorId(int creatorId) {
+        this.creatorId = creatorId;
+    }
+    
+    public String getWorkTitle() {
+        return workTitle;
+    }
+    
+    public void setWorkTitle(String workTitle) {
+        this.workTitle = workTitle;
+    }
+    
+    public String getCreatorName() {
+        return creatorName;
+    }
+    
+    public void setCreatorName(String creatorName) {
+        this.creatorName = creatorName;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        Connection that = (Connection) o;
+        
+        return id == that.id;
+    }
+    
+    @Override
+    public int hashCode() {
+        return id;
     }
 }
